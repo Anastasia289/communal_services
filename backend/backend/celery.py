0d2abebba68c9452ed383a5a84047ca965944979
Anastasia <bogdanovanastja@yandex.ru>
2024-07-24 01:28:11 +0300

    4

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,11 +1,13 @@
+from __future__ import absolute_import, unicode_literals
+
 import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-sched = Celery("idp")
+app = Celery("backend")
 
-sched.config_from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
-sched.autodiscover_tasks()
+app.autodiscover_tasks()
