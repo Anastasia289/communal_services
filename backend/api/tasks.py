2c46a57b42d81e6e1f9eea4d4208166997c2ed3d
Anastasia <bogdanovanastja@yandex.ru>
2024-07-24 00:48:34 +0300

    3

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,8 +1,11 @@
 from celery.task import periodic_task
 
+# from services.models import WaterMeterData
+
 
 @periodic_task(
     ignore_result=True, run_every=30
 )  # 10 секунд, или timedelta(seconds=10)
 def just_print():
+    # d = WaterMeterData.objects.filter
     print("Print from celery task")
