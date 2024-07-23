e26e102cce6b5ecda15ba66776741fa837ab678b
Anastasia <bogdanovanastja@yandex.ru>
2024-07-23 23:25:36 +0300

    1

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -4,8 +4,8 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "communal_services.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-app = Celery("communal_services")
+app = Celery("backend")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
