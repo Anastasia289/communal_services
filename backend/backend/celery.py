2758033e32b6bc86336ad35283ca3279e5e25e0b
Anastasia <bogdanovanastja@yandex.ru>
2024-07-24 00:07:26 +0300

    1

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,11 +1,13 @@
-from __future__ import absolute_import, unicode_literals
-
 import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "information_system_gkh.settings"
+)
 
-app = Celery("backend")
+app = Celery("information_system_gkh")
+
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
 app.autodiscover_tasks()
