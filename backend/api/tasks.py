fe3110bf38023f7176b3796757949cf29cd2e74f
Anastasia <bogdanovanastja@yandex.ru>
2024-07-24 10:42:13 +0300

    h

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,14 +1,24 @@
-# import eventlet
-from celery.task import periodic_task
+# from celery import Celery
+# from celery.schedules import crontab
 
-# eventlet.monkey_patch()
-
-# from services.models import WaterMeterData
+# app = Celery()
 
 
-@periodic_task(
-    ignore_result=True, run_every=30
-)  # 10 секунд, или timedelta(seconds=10)
-def just_print():
-    # d = WaterMeterData.objects.filter
-    print("Print from celery task")
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     sender.add_periodic_task(10.0, test.s("hello"), name="add every 10")
+
+#     # Calls test('hello') every 30 seconds.
+#     # It uses the same signature of previous task, an explicit name is
+#     # defined to avoid this task replacing the previous one defined.
+#     sender.add_periodic_task(30.0, test.s("hello"), name="add every 30")
+
+#     # Calls test('world') every 30 seconds
+#     sender.add_periodic_task(30.0, test.s("world"), expires=10)
+
+#     # Executes every Monday morning at 7:30 a.m.
+#     sender.add_periodic_task(
+#         crontab(hour=7, minute=30, day_of_week=1),
+#         test.s("Happy Mondays!"),
+#     )
